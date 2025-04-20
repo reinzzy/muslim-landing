9331dd99a28145419742073ed06015fb5273cd07
Otto <ottosantoso11@gmail.com>
2025-04-20 14:16:56 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4CAF50",
+        secondary: "#FF9800",
+      },
+    },
+  },
+  plugins: [],
+};
